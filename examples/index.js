47d625715f0c3ccdb68ab14237f52f16a0cc2e6b
entom <tomasz@entom.info>
2021-06-29 22:54:08 +0200

    feat(server): add more example of routing

--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,11 @@
 const server = require('../server')
 const app = server()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.get('/user/:id/profile', (req, res) => {
+  console.log('user-get-profile')
+  res.html('<h1>Profile</h1>')
+})
 
 app.get('/user/:id', (req, res) => {
   console.log('user-get')

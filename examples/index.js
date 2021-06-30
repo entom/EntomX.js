e6286b7401e3d1d13797549cf93a4a6c2f583147
entom <tomasz@entom.info>
2021-06-30 22:06:24 +0200

    feat(server): example usage of middleware for route

--- examples/index.js
+++ examples/index.js
@@ -2,6 +2,11 @@ const server = require('../server')
 const app = server()
 const port = process.env.PORT || 3000
 
+const middle = (req, res, next) => {
+  console.log('middle ---')
+  next()
+}
+
 app.get('/user/:id/profile', (req, res) => {
   console.log('user-get-profile')
   res.html('<h1>Profile</h1>')
@@ -14,6 +19,10 @@ app.get('/user/:id', (req, res) => {
 
 app.post('/user', (req, res) => {
 
+})
+
+app.get('/user', middle, (req, res) => {
+  res.json({status: 'ok', page: 'user'})
 })
 
 app.listen(port, () => {
